a089760eb56da5ef3cafc7c857aa12c42dacbaf5
anuLoveCoading <anusrivas07@gmail.com>
2022-10-16 21:24:00 +0530

    hcf-class

--- hcf.java
+++ hcf.java
@@ -3,18 +3,35 @@ public class hcf {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        System.out.println("Enter Your First-Number");
-        int A = scn.nextInt();
-        System.out.println("Enter Your Second-Number");
-        int B = scn.nextInt();
-        int h1=0;
-        for(int i=1;i<=A || i<=B ;i++){
-            if(A%i==0 && B%i==0){
-                h1 = i;
-            }
+        // System.out.println("Enter Your First-Number");
+        // int A = scn.nextInt();
+        // System.out.println("Enter Your Second-Number");
+        // int B = scn.nextInt();
+        // int h1=0;
+        // for(int i=1;i<=A || i<=B ;i++){
+        //     if(A%i==0 && B%i==0){
+        //         h1 = i;
+        //     }
+        // }
+        // // System.out.println("Your HCF is: ");
+        // System.out.println("Your HCF is: "+h1);
+
+
+        int a = 78945;
+        int sum =0;
+        while(a>0){
+            int d = a%10;
+             sum+=d;
+             a/=10;
         }
-        // System.out.println("Your HCF is: ");
-        System.out.println("Your HCF is: "+h1);
+        System.out.println(sum);
+
+
+
+
+
+
+
 
     }
 }
