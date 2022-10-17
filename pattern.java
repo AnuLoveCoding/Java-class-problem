2468595fa1e1960bead23fe2e440e726178d7cba
anuLoveCoading <anusrivas07@gmail.com>
2022-10-17 15:04:01 +0530

    pattern-class

---
+++ pattern.java
@@ -0,0 +1,16 @@
+/**
+ * pattern
+ */
+import java.util.*;;
+public class pattern {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        String bag= "";
+        for(int i=10;i>0;i--){
+            bag+="*"+" ";
+            System.out.println(bag);
+        }
+    }
+}
