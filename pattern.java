4690674034913a1134594092f893abc817d3c2a6
anuLoveCoading <anusrivas07@gmail.com>
2022-10-18 23:46:33 +0530

    pattern-class

--- pattern.java
+++ pattern.java
@@ -41,25 +41,34 @@ public class pattern {
         // }
 
 
-        for(int i = 1;i<=5;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print("*\t");
+        // for(int i = 1;i<=5;i++){
+        //     for(int j=5;j>=i;j--){
+        //         System.out.print("*\t");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for(int i=5;i>0;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*\t");
+        //     }
+        //    System.out.println();
+        // }
+
+
+
+
+            for(int i=1;i<=10;i++){
+                for(int j=1;j<=i;j++){
+                    if(j%2==0){
+                        System.out.print(" "+j);
+                    }else{
+                        System.out.print(" *");
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-
-
-        for(int i=5;i>0;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*\t");
-            }
-           System.out.println();
-        }
-
-
-
-
-
 
 
 
