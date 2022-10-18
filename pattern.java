f460839ab84243308e4881998cdd387ecd455605
anuLoveCoading <anusrivas07@gmail.com>
2022-10-18 17:59:45 +0530

    Pattern

--- pattern.java
+++ pattern.java
@@ -33,14 +33,20 @@ public class pattern {
 
        
         //    Star printing Concepts// Using nested Loop;
-        for(int i=1;i<=15;i++){
-            for(int j=1;j<=6;j++){
-                System.out.print("* ");
+        // for(int i=1;i<=15;i++){
+        //     for(int j=1;j<=6;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        for(int i = 1;i<=6;i++){
+            for(int j=6;j>i;j--){
+                System.out.print("*\t");
             }
             System.out.println();
         }
-
-
 
 
 
