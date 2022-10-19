6001f774b516076e916df2b79d80884ce2a3feaf
anuLoveCoading <anusrivas07@gmail.com>
2022-10-19 19:41:54 +0530

    Pattern-class

--- pattern.java
+++ pattern.java
@@ -21,7 +21,7 @@ public class pattern {
         // }
        
 
-        //  print the pattern 
+        //  print the  stair pattern printing
 
         // for (int i = 1; i <= 5; i++) {
         //     for (int j = 1; j <= i; j++) {
@@ -49,16 +49,16 @@ public class pattern {
         // }
 
             // reverse stair case star printing;
-            for(int i=5;i>0;i--){
-                for(int j=1;j<=i;j++){
-                    System.out.print("*\t");
-                }
-            System.out.println();
-            }
+            // for(int i=5;i>0;i--){
+            //     for(int j=1;j<=i;j++){
+            //         System.out.print("*\t");
+            //     }
+            // System.out.println();
+            // }
 
 
 
-
+            // print the even_number with star Printing;
             // for(int i=1;i<=10;i++){
             //     for(int j=1;j<=i;j++){
             //         if(j%2==0){
@@ -70,21 +70,56 @@ public class pattern {
             //     System.out.println();
             // }
 
-                // reverse stair case star printing;
-            for(int i=1;i<=5;i++){
-                for(int j=1;j<=5-i;j++){
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
+                // reverse left stair case star printing;
+                // for(int i=1;i<=5;i++){
+                //     for(int j=1;j<=5-i;j++){
+                //         System.out.print("*");
+                //     }
+                //     System.out.println();
+                // }
 
 
+                // for(int i=1;i<=5;i++){
+                //     System.out.println("*");
+
+                //     for(int j=1;j<=)
+                // }
+
+                // reverse right stair case star printing;
+                    int n = 6;
+                    int nspaces = n-1;
+                    int nstars =1;
+
+                    for(int i=1;i<=n;i++){
+                        for(int j=1;j<=nspaces;j++){
+                            System.out.print("\t");
+                        }
+                        for(int j=1;j<=nstars;j++){
+                            System.out.print("*\t");
+                        }
+                        nspaces--;
+                        nstars++;
+                        System.out.println();
+                    }
 
 
-
-
-
-
+                // int n =5;
+                // int nspaces = n - 1;
+                // int nstars = 1;
+        
+                // for (int i = 1; i <= n; i++) {
+                //     for (int j = 1; j <= nspaces; j++) {
+                //         System.out.print("\t");
+                //     }
+        
+                //     for (int j = 1; j <= nstars; j++) {
+                //         System.out.print("*\t");
+                //     }
+        
+                //     nspaces--;
+                //     nstars++;
+                //     System.out.println();
+                // }
 
 
 
